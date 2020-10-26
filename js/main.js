6b4b2ef16183ce601fc11f45ba64078a87113d25
--replace-all <dgosan37@gmail.com>
2020-10-26 14:48:41 +0200

    Add the collapsed look of the navbar

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ let mainSearch = document.getElementById("mainSearch");
 let mainSearchGroup = document.getElementById("mainSearchGroup");
 
 mainSearch.addEventListener("focus",function () {
-    mainSearchGroup.style.width = "30%";
+    mainSearchGroup.classList.add("col-md-4");
+    mainSearchGroup.classList.remove("col-md-2");
 })
 
 mainSearch.addEventListener("blur",function () {
-    mainSearchGroup.style.width = "18%";
+    mainSearchGroup.classList.add("col-md-2");
+    mainSearchGroup.classList.remove("col-md-4");
 })
