dcb9a5f3e578df28a238b88af25f23247f8e7da7
--replace-all <dgosan37@gmail.com>
2020-11-02 00:14:58 +0200

    Upgrade the navbar and add the site footer

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 let mainSearch = document.getElementById("mainSearch");
 let mainSearchGroup = document.getElementById("mainSearchGroup");
+let mainSearchIcon = document.getElementById("mainSearchIcon");
 
 mainSearch.addEventListener("focus",function () {
     mainSearchGroup.classList.add("col-md-4");
     mainSearchGroup.classList.remove("col-md-2");
+    mainSearchIcon.style.backgroundColor = "#fff";
+    mainSearchIcon.style.color = "#22766E";
 })
 
 mainSearch.addEventListener("blur",function () {
     mainSearchGroup.classList.add("col-md-2");
     mainSearchGroup.classList.remove("col-md-4");
+    mainSearchIcon.style.backgroundColor = "#ED4A43";
+    mainSearchIcon.style.color = "#fff";
 })
