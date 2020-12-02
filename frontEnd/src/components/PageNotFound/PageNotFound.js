c9cd1bd6cf5f7764a93c9037ee0ca196babf7184
xOooQOFU <xOooQOFU>
2020-12-02 17:47:20 +0200

    Page not found refactoring

--- frontEnd/src/components/PageNotFound/PageNotFound.js
+++ frontEnd/src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Button} from "react-bootstrap";
 import {Header} from "../Header/Header";
 import {useHistory} from "react-router";
-import './styles.css';
 
 export const PageNotFound = () => {
     const history = useHistory();
@@ -13,9 +11,6 @@ export const PageNotFound = () => {
                 <p className="h1 my-4">Page Not Found</p>
             </div>
             <div className="row justify-content-center">
-            <Button onClick={() => history.push("/books")} className="btn-lg" id="buttonGoShopping">
-                Go shopping
-            </Button>
         </div>
         </div>
     );
